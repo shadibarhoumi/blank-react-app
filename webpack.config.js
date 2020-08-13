0d9ea8fd8af6e71c5f8938ff1baa803dcabe7eb6
Shadi Barhoumi <shadibarhoumi@gmail.com>
2020-08-12 23:24:54 -0700

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: './app/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index_bundle.js',
+    publicPath: '/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        use: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+  devServer: {
+    host: '0.0.0.0',
+    historyApiFallback: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'app/index.html',
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: '_redirects',
+        },
+      ],
+    }),
+  ],
+};
